a7627a8ba72f73679554a303a59ddf92e6310494
Tarik <49348194+tarizer@users.noreply.github.com>
2019-12-22 19:19:35 +0100

    Add 2 excercices: 3.2 and 3.3

---
+++ javascript/3.3 - Musical Groups.js
@@ -0,0 +1,42 @@
+/*
+ * Programming Quiz: Musical Groups (3-3)
+
+Write a series of conditional statements that:
+
+Prints "not a group" if musicians is less than or equal to 0
+Prints "solo" if musicians is equal to 1
+Prints "duet" if musicians is equal to 2
+Prints "trio" if musicians is equal to 3
+Prints "quartet" if musicians is equal to 4
+Prints "this is a large group" if musicians is greater than 4
+
+ */
+
+// change the value of `musicians` to test your conditional statements
+let musicians = "toto";
+let message = "";
+
+// your code goes here
+
+if (!Number.isInteger(musicians) || musicians <= 0 ) {
+  message = "not a group";
+} else {
+  switch (musicians) {
+    case 1:
+      message = "solo";
+      break;
+    case 2:
+      message = "duet";
+      break;
+    case 3:
+      message = "trio";
+      break;
+    case 4:
+      message = "quartet";
+      break;
+    default:
+      message = "this is a large group";
+  }
+}
+
+console.log(message);
