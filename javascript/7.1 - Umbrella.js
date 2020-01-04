a22974128d14d5832d072c16cb23f0498feacbd2
Tarik <49348194+tarizer@users.noreply.github.com>
2020-01-04 10:00:20 +0100

    Add eslint/Pretier and fix some files

--- javascript/7.1 - Umbrella.js
+++ javascript/7.1 - Umbrella.js
@@ -2,19 +2,21 @@
  * Programming Quiz: Umbrella (7-1)
  */
 
-var umbrella = {
-  color: "pink",
+const umbrella = {
+  color: 'pink',
   isOpen: true,
+  // eslint-disable-next-line object-shorthand
   open: function() {
-      if (umbrella.isOpen === true) {
-          return "The umbrella is already opened!";
-      } else {
-          umbrella.isOpen = true;
-          return "Julia opens the umbrella!";
-      }
+    if (umbrella.isOpen === true) {
+      return 'The umbrella is already opened!';
+    }
+    umbrella.isOpen = true;
+    return 'Julia opens the umbrella!';
   },
-  close: (() => (umbrella.isOpen === true) ? (umbrella.isOpen = false,  "Julia closes the umbrella!") : "The umbrella is already closed!")
-
+  close: () =>
+    umbrella.isOpen === true
+      ? ((umbrella.isOpen = false), 'Julia closes the umbrella!')
+      : 'The umbrella is already closed!',
 };
 
 console.log(umbrella.open());
--- javascript/7.4 - Facebook Friends.js
+++ javascript/7.4 - Facebook Friends.js
@@ -1,22 +1,22 @@
+/* eslint-disable no-plusplus */
 /*
  * Programming Quiz: Facebook Friends (7-5)
  */
 
 // your code goes here
 
-facebookProfile = {
-  name: "Tarik",
+const facebookProfile = {
+  name: 'Tarik',
   friends: 5,
-  messages: ["message1", "message2", "message3"],
+  messages: ['message1', 'message2', 'message3'],
 
-  postMessage: (message) => facebookProfile.messages.push(message),
-  deleteMessage: (index) => facebookProfile.messages.splice(index,1),
+  postMessage: message => facebookProfile.messages.push(message),
+  deleteMessage: index => facebookProfile.messages.splice(index, 1),
   addFriend: () => ++facebookProfile.friends,
-  removeFriend: () => --facebookProfile.friends
+  removeFriend: () => --facebookProfile.friends,
+};
 
-}
-
-console.log(facebookProfile.postMessage("hello!"));
+console.log(facebookProfile.postMessage('hello!'));
 console.log(facebookProfile);
 
 console.log(facebookProfile.deleteMessage(2));
